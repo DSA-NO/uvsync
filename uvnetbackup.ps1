bf84ec0ff6d6b76eaf4bee98034e41d0af6e38fb
bytting <dag.robole@gmail.com>
2022-09-27 08:20:22 +0200

    Updated backup script

--- uvnetbackup.ps1
+++ uvnetbackup.ps1
@@ -8,24 +8,13 @@ $pathFrom = "D:\UV\outbox"
 # Katalog der sikkerhetskopi skal lagres
 $pathTo = "D:\UV\backup"
 
-# Iterer katalog som skal sikkerhetskopieres
-Get-ChildItem -Path $pathFrom | Foreach-Object { 
-	
-	# Opprett navn på destinasjonskatalog
-	$destDir = $pathTo + "\" + $_.Name + "\"
-	
-	# Opprett destinasjonskatalog hvis den ikke finnes allerede
-	if(!(Test-Path $destDir)) { 
-	
-		New-Item $destDir -ItemType Directory -ErrorAction Stop
-	}
-	
-	# Opprett liste over CSV filer som skal kopieres
-	$files = Get-ChildItem -Path $_.FullName | Where-Object { $_.Name -match '.csv$' }	
-	
-	# Flytt CSV filer til destinasjon
-	foreach ($file in $files) { 
-	
-		Move-Item $file.FullName -Destination $destDir -ErrorAction SilentlyContinue 
-	}	
+$files = @(Get-ChildItem -Path "$($pathFrom)" -File -Recurse)
+$directories = @(Get-ChildItem -Path "$($pathFrom)" -Directory -Recurse)
+
+ForEach($directory in $directories) {
+	New-Item ($directory.FullName).Replace("$($pathFrom)", $pathTo) -ItemType Directory -ea SilentlyContinue | Out-Null
+}
+
+ForEach($file in $files) {	
+	Move-Item -Path $file.FullName -Destination $file.FullName.Replace("$($pathFrom)", $pathTo) -Force -ErrorAction SilentlyContinue
 }
